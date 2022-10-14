bf037265aa8de7bb4e85e7fee087c52548f0c405
ilkerdurmaz <ilker.prime@gmail.com>
2022-10-14 20:06:17 +0300

    button texts centered

--- src/components/Cookies/Button.vue
+++ src/components/Cookies/Button.vue
@@ -23,6 +23,6 @@ const clickHandler = () => {
 
 <style lang="postcss" scoped>
 .button {
-	@apply whitespace-nowrap border-2 border-tertiary  rounded-lg px-3 py-1 bg-secondary text-quaternary hover:bg-tertiary hover:text-primary font-bold hover:border-quaternary col-span-1 lg:col-span-3 xl:col-span-1 transition-all;
+	@apply whitespace-nowrap border-2 border-tertiary  rounded-lg px-3 py-1 bg-secondary text-quaternary hover:bg-tertiary hover:text-primary font-bold hover:border-quaternary col-span-1 lg:col-span-3 xl:col-span-1 transition-all flex justify-center;
 }
 </style>
